d40f561b0ff8cfbadca53b06be70fca09a53142b
SHIVANGI RANI VERMA <86093851+gitSRVerma@users.noreply.github.com>
2022-11-23 18:07:31 +0530

    CAFENA UPADTED

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+// this above code signifies..when the menu button is clicked 
+// the navbar data will visible which will be encapsulated inside it.
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
